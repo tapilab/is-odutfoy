0f7a1f7f3f6470b3d314e6433901071819f560ad
odutfoy <dutfoy.olivier@gmail.com>
2016-03-07 10:23:46 -0600

    added a few fields to data retrieval

--- src/getdata.py
+++ src/getdata.py
@@ -4,6 +4,7 @@ import requests
 import json
 import pickle
 import os
+from time import sleep
 
 #player_list = curl('http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2013-14&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.85 Chrome/45.0.2454.85 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Referer: http://stats.nba.com/league/player/' -H 'Cookie: ug=564bc5cb06dd690a3c852e7da205ef8b; ugs=1; _gat=1; _ga=GA1.2.1028222052.1454632808; crtg_trnr=; s_cc=true; s_vi=[CS]v1|2B1C901605010772-40000146E0028408[CE]; s_sq=nbag-n-league%3D%2526pid%253Dstats.nba.com%25253A%25252Fplayer%25252F%2526pidt%253D1%2526oid%253Dhttp%25253A%25252F%25252Fstats.nba.com%25252Fplayer%25252F%2526ot%253DA; s_fid=611F6CAB17F03E6A-0F7BD99682C4658E' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' --compressed)
 
@@ -50,16 +51,33 @@ def get_data(season):
         print "fetching stats of player %d" % (i + 1)
 
         name_req = requests.request('GET', 'http://stats.nba.com/stats/commonplayerinfo?LeagueID=00&PlayerID=' + str(player[0]) + '&SeasonType=Regular+Season', headers=headers, cookies=cookies)
+        sleep(1)
 
         player_info['name'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][3]
+        player_info['birthdate'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][6]
+        player_info['start_year'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][22]
+
+        #may be interesting but incomplete for previous seasons
+        # player_info['height'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][10]
+        # player_info['weight'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][11]
+        # player_info['position'] = json.loads(name_req.content)['resultSets'][0]['rowSet'][0][14]
 
         stats_req = requests.request('GET', 'http://stats.nba.com/stats/playergamelog?LeagueID=00&PlayerID=' + str(player[0]) + '&Season=' + season + '&SeasonType=Regular+Season', headers=headers, cookies=cookies)
+        sleep(1)
+
 
         player_info['stats'] = []
 
         #removing useless data
         for match in json.loads(stats_req.content)['resultSets'][0]['rowSet']:
             player_info['stats'].append(match[2:])
+
+        #adding additionnal data (age and experience)
+        experience = int(season.split('-')[0]) - player_info['start_year']
+        age = int(season.split('-')[0]) - int(player_info['birthdate'].split('-')[0]) #approximation
+
+        player_info['stats'].append(age)
+        player_info['stats'].append(experience)
 
         pickle.dump(player_info, open('data' + os.sep + season + os.sep + 'player_stats' + os.sep + str(player[0]) + '.pkl', 'wb'))
 
