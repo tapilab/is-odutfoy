cb418fdae3cf7c1478e898c99f61a58956e44242
odutfoy <dutfoy.olivier@gmail.com>
2016-04-03 23:27:07 -0500

    started working on sliding training and implemented a pseudo k-fold error

--- src/model.py
+++ src/model.py
@@ -18,7 +18,7 @@ def raw_averages(season):
 
     return X, y
 
-#sliding error using raw averages for one player
+#sliding average using raw averages for one player
 def sliding_raw_average(season, playerID):
     averages = []
     next_match_points = []
@@ -34,6 +34,26 @@ def sliding_raw_average(season, playerID):
 
     return X, y
 
+#sliding averages for all players in all seasons
+def sliding_raw_averages(seasons):
+    averages = []
+    next_match_points = []
+    for season in seasons:
+        players = glob.glob('data' + os.sep + season + os.sep + 'player_stats' + os.sep + "*.pkl")
+        for file in players:
+            playerID = file[26:-4]
+            player = pickle.load(open('data' + os.sep + season + os.sep + 'player_stats' + os.sep + playerID + '.pkl', 'rb'))
+            games_num = len(player['stats'])
+
+            for i in range(1, games_num - 1):
+                averages.append(average(season, playerID, i)[0])
+                next_match_points.append(compute_fantasy(season, playerID, i + 1))
+
+    X = np.array(averages)
+    y = np.array(next_match_points)
+
+    return X, y
+
 
 def train_linear(X, y, normalize = False):
     lr = linear_model.LinearRegression(normalize=normalize)
@@ -42,13 +62,13 @@ def train_linear(X, y, normalize = False):
     return lr
 
 #X, y = raw_averages('2011-12')
-X, y = sliding_raw_average('2011-12', '101107')
-print X.shape, y.shape
-model = train_linear(X, y)
-modeln = train_linear(X, y, True)
+# X, y = sliding_raw_average('2011-12', '101107')
+# print X.shape, y.shape
+# model = train_linear(X, y)
+# modeln = train_linear(X, y, True)
 
 
-#computed error given model an input
+#computed error given model as input
 def error(model, X, y):
     predictions = model.predict(X)
     avg_error = 0.
@@ -60,13 +80,40 @@ def error(model, X, y):
         max_error = error if error > max_error else max_error
         errors.append(error)
 
-    plt.plot(errors)
-    plt.show()
+    #plt.plot(errors)
+    #plt.show()
 
     return avg_error/predictions.shape[0], max_error
 
-print error(model, X, y)
-print error(modeln, X, y)
+#all but one fold error over seasons using sliding raw averages
+def ABOF_error(seasons):
+    errors = []
+    avg_error = 0.
+
+    for season in seasons:
+        print "Testing on season %s (Training on the rest)" % season
+        tmp = list(seasons)
+        tmp.remove(season)
+        trainX, trainy = sliding_raw_averages(tmp)
+        model = train_linear(trainX, trainy)
+        testX, testy = sliding_raw_averages([season])
+        error = error(model, testX, testy)[0]
+        errors.append(error)
+        avg_error += error
+
+        print "average error is %f" % error
+
+    result = avg_error/len(season)
+    print "Average error over all seasons is %f" % result
+
+    return result
+
+# print error(model, X, y)
+# print error(modeln, X, y)
+
+seasons = ['2006-07', '2007-08', '2008-09', '2009-10', '2010-11', '2011-12', '2012-13', '2013-14']
+
+ABOF_error(seasons)
 
 
 
